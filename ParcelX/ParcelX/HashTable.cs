8bf4d19ac0f5bb4c341c28980a625b9f5a53e812
OubaidM <maudarbux.oubaid@gmail.com>
2025-03-17 19:18:44 +0400

    Switched over to prod branch and created HashTable.cs

---
+++ ParcelX/ParcelX/HashTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostalCW.DataStructures
+{
+    public class HashTable<T>
+    {
+        private readonly int size = 100;
+        private List<KeyValuePair<int, T>>[] buckets;
+
+        public HashTable()
+        {
+            buckets = new List<KeyValuePair<int, T>>[size];
+            for (int i = 0; i < size; i++)
+            {
+                buckets[i] = new List<KeyValuePair<int, T>>();
+            }
+        }
+
+    }
+}
